19a0858641775c4d51f3771bf1524306ef08d19c
ldl093 <sugimoto@liddell.tokyo>
2021-01-14 18:21:00 +0900

    navigation/stack/tab

--- App.tsx
+++ App.tsx
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import {HomeScreen} from './src/screens/HomeScreen';
 import {LandingScreen} from './src/screens/LandingScreen';
 
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+const switchNavigator = createSwitchNavigator({
+  landingStack: {
+    screen: createStackNavigator({
+        Landing: LandingScreen
+    },{
+      defaultNavigationOptions: {
+        headerShown: false,
+      }
+    })
+  },
+  homeStack: createBottomTabNavigator({
+    // Home tab Icon
+    Home: {
+      screen: createStackNavigator({
+        HomePage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon}/>
+        }
+      }
+    },
+    Offer: {
+      screen: createStackNavigator({
+        OfferPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/offer_icon.png') : require('./src/images/offer_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon}/>
+        }
+      }
+    },
+    Cart: {
+      screen: createStackNavigator({
+        CartPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/cart_icon.png') : require('./src/images/cart_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon}/>
+        }
+      }
+    },
+    Account: {
+      screen: createStackNavigator({
+        AccountPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({focused, tintColor}) => {
+          let icon = focused == true ? require('./src/images/account_icon.png') : require('./src/images/account_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon}/>
+        }
+      }
+    }
+  })
+})
+
+const AppNavigation = createAppContainer(switchNavigator)
+
 export default function App() {
   return (
-    <LandingScreen/>
+    <AppNavigation/>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tabIcon: {
+    width: 30,
+    height: 30
   },
 });
